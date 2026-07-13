21d66234bceb441b377f88b8872296f2e45b2669
saul-castillo <saulandrewcastillo@gmail.com>
2026-07-13 06:51:31 -0400

    Test extracted shared-net summaries

--- designs/scripts/matchmaker/tests/test_spice_inspector.py
+++ designs/scripts/matchmaker/tests/test_spice_inspector.py
@@ -25,6 +25,28 @@ R1 D S 1k
         self.assertEqual(len(parsed["demo"].subcircuit_instance_statements), 1)
         self.assertIn("X1 OUT IN VSS child", parsed["demo"].statements)
 
+        instance = parsed["demo"].subcircuit_instances[0]
+        self.assertEqual(instance.name, "X1")
+        self.assertEqual(instance.nodes, ("OUT", "IN", "VSS"))
+        self.assertEqual(instance.subcircuit_name, "child")
+
+    def test_shared_instance_nets_report_multi_instance_connections(self):
+        netlist = """
+.subckt top
+X1 route a child
+X2 route b child
+X3 route c child
+X4 other d child
+.ends top
+.subckt child A B
+.ends child
+"""
+        top = parse_spice_subcircuits(netlist)["top"]
+        self.assertEqual(
+            top.shared_instance_nets(minimum_instance_count=2),
+            {"route": ("X1", "X2", "X3")},
+        )
+
     def test_unterminated_subcircuit_fails(self):
         with self.assertRaises(ValueError):
             parse_spice_subcircuits(".subckt demo A B\nR1 A B 1k\n")
