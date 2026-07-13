0ed4d9a259c797641292c3615acd2ceeb844bd3d
saul-castillo <saulandrewcastillo@gmail.com>
2026-07-13 06:17:50 -0400

    Add SPICE netlist inspection package

---
+++ designs/scripts/matchmaker/src/matchmaker/verification/netlist/__init__.py
@@ -0,0 +1,8 @@
+"""SPICE netlist inspection helpers."""
+
+from matchmaker.verification.netlist.spice_inspector import (
+    SpiceSubcircuit,
+    parse_spice_subcircuits,
+)
+
+__all__ = ["SpiceSubcircuit", "parse_spice_subcircuits"]
