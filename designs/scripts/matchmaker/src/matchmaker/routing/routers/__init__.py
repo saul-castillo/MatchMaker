70f6b643fd2562eee8c88a96aa8d1d9a9de88de8
saul-castillo <saulandrewcastillo@gmail.com>
2026-07-15 12:42:57 -0400

    Export route plan executor

--- designs/scripts/matchmaker/src/matchmaker/routing/routers/__init__.py
+++ designs/scripts/matchmaker/src/matchmaker/routing/routers/__init__.py
@@ -1,3 +1,13 @@
-"""
-Physical route execution adapters.
-"""
+"""Physical route execution adapters."""
+
+from matchmaker.routing.routers.route_plan_executor import (
+    ExecutedRoutePlan,
+    build_route_plan_geometry,
+    execute_route_plan,
+)
+
+__all__ = [
+    "ExecutedRoutePlan",
+    "build_route_plan_geometry",
+    "execute_route_plan",
+]
