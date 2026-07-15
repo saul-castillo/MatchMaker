812c0ecfd5915bc21eab3de9f675782d5b4536cb
saul-castillo <saulandrewcastillo@gmail.com>
2026-07-15 12:39:15 -0400

    Add routing plan package

---
+++ designs/scripts/matchmaker/src/matchmaker/routing/plans/__init__.py
@@ -0,0 +1,17 @@
+"""Typed route-plan intermediate representation."""
+
+from matchmaker.routing.plans.route_plan import (
+    ConstraintCheck,
+    RouteMetrics,
+    RoutePlan,
+    RouteSegment,
+    ViaPlan,
+)
+
+__all__ = [
+    "ConstraintCheck",
+    "RouteMetrics",
+    "RoutePlan",
+    "RouteSegment",
+    "ViaPlan",
+]
