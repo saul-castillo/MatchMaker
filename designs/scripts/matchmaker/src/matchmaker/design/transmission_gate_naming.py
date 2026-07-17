9acc0442887eb25d06ec14cb2e9272b641222934
saul-castillo <saulandrewcastillo@gmail.com>
2026-07-16 23:47:11 -0400

    Separate transmission-gate naming from geometry

---
+++ designs/scripts/matchmaker/src/matchmaker/design/transmission_gate_naming.py
@@ -0,0 +1,6 @@
+"""Stable logical names shared by transmission-gate pipeline stages."""
+
+NMOS_INSTANCE_NAME = "NMOS"
+PMOS_INSTANCE_NAME = "PMOS"
+
+__all__ = ["NMOS_INSTANCE_NAME", "PMOS_INSTANCE_NAME"]
