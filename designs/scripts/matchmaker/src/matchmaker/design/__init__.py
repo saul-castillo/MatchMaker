d94e3e4714e07f255357bee791967d1b32090a22
saul-castillo <saulandrewcastillo@gmail.com>
2026-07-16 22:42:58 -0400

    Add typed circuit-design package

---
+++ designs/scripts/matchmaker/src/matchmaker/design/__init__.py
@@ -0,0 +1,13 @@
+"""Typed circuit hierarchy and logical connectivity independent of schematics."""
+
+from matchmaker.design.circuit_manifest import (
+    CircuitInstance,
+    CircuitManifest,
+    CircuitNet,
+)
+
+__all__ = [
+    "CircuitInstance",
+    "CircuitManifest",
+    "CircuitNet",
+]
