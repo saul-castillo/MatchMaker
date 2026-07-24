2fb5ff878923781ae7efdfd389ed3f3caf3b09cc
saul-castillo <saulandrewcastillo@gmail.com>
2026-07-24 07:22:20 -0400

    Export canonical CDAC demo cell names

--- designs/scripts/matchmaker/src/matchmaker/outputs/__init__.py
+++ designs/scripts/matchmaker/src/matchmaker/outputs/__init__.py
@@ -1,6 +1,12 @@
+from .cdac_demo_cell_names import (
+    GF180_CDAC_B0_REFERENCE_SELECTOR_DEMO_CELL_NAME,
+    GF180_CDAC_BASE_TRANSMISSION_GATE_DEMO_CELL_NAME,
+)
 from .core_analog_cell_paths import CoreAnalogCellPaths, create_core_analog_cell_paths
 
 __all__ = [
     "CoreAnalogCellPaths",
+    "GF180_CDAC_B0_REFERENCE_SELECTOR_DEMO_CELL_NAME",
+    "GF180_CDAC_BASE_TRANSMISSION_GATE_DEMO_CELL_NAME",
     "create_core_analog_cell_paths",
 ]
